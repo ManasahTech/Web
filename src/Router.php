a9f94d4e16a8bbc972d192920757f499fc5f974f
Abdullah Sowailem <abdullah.sowailem@gmail.com>
2024-06-11 14:29:37 +0300

    Fixing issues.

---
+++ src/Router.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace ManasahTech\Data\MariaDB;
+
+use ManasahTech\Container\Attributes\Injectable;
+use ManasahTech\Container\Scopes\SingletonScope;
+
+
+#[Injectable(scope: SingletonScope::class)]
+class Router
+{
+}
